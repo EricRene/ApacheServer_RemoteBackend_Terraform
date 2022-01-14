f369a49224c6e29bb225e3f24a8e9fc2f8b736bc
Eric Henriquez <eric_henriquez@epam.com>
2022-01-14 16:07:22 -0600

    Modified user data

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -237,18 +237,21 @@ resource "aws_instance" "web" {
 
 
   user_data = <<EOF
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install apache2 unzip -y
-echo '<html><center><body bgcolor="black" text="#39ff14" style="font-family: Arial"><h1>Load Balancer Demo</h1><h3>This is a test container! > /var/www/html/index.html
-curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/index.html
-echo '</h3> <h3>Instance Id: ' >> /var/www/html/index.html
-curl http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html
-echo '</h3> <h3>Public IP: ' >> /var/www/html/index.html
-curl http://169.254.169.254/latest/meta-data/public-ipv4 >> /var/www/html/index.html
-echo '</h3> <h3>Local IP: ' >> /var/www/html/index.html
-curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html
-echo '</h3></html> ' >> /var/www/html/index.html
+  sudo apt-get update -y
+  sudo apt-get install apache2 unzip -y
+  sudo chgrp -R www-data /var/www/html
+  sudo gpasswd -a ubuntu www-data
+  sudo chmod -R 777 /var/www/html
+  echo '<html><center><body bgcolor="black" text="#39ff14" style="font-family: Arial"><h1>Load Balancer Demo</h1><h3>Availability Zone: ' > /var/www/html/index.html
+  curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/index.html
+  echo '</h3> <h3>Instance Id: ' >> /var/www/html/index.html
+  curl http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html
+  echo '</h3> <h3>Public IP: ' >> /var/www/html/index.html
+  curl http://169.254.169.254/latest/meta-data/public-ipv4 >> /var/www/html/index.html
+  echo '</h3> <h3>Local IP: ' >> /var/www/html/index.html
+  curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html
+  echo '</h3></html> ' >> /var/www/html/index.html
+
 
 EOF
 
