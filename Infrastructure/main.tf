f9a13bcc61830a00f57d59fa1cb93558d658b05a
Eric Henriquez <eric_henriquez@epam.com>
2022-01-14 13:19:21 -0600

    Modified instance resource

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -224,11 +224,11 @@ resource "aws_lb_target_group" "my_target_group" {
 
 resource "aws_instance" "web" {
   count = 1
-  
+
   ami                    = "ami-001089eb624938d9f"
   instance_type          = "t2.micro"
   subnet_id              = "192.168.${count.index}.0/24"
-  vpc_security_group_ids = aws_security_group.web_server_sg.id
+  vpc_security_group_ids = "aws_security_group.web_server_sg.id"
 
   user_data = <<EOF
 #!/bin/bash
