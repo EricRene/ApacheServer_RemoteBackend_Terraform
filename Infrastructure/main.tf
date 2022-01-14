550dc696f51404fa70a132ae3114b0d967c72f6e
Eric Henriquez <eric_henriquez@epam.com>
2022-01-14 16:27:17 -0600

    Modified user data

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -237,23 +237,12 @@ resource "aws_instance" "web" {
 
 
   user_data = <<EOF
-  #!/bin/bash
-  sudo apt-get update -y
-  sudo apt-get install apache2 unzip -y
-  sudo chgrp -R www-data /var/www/html
-  sudo gpasswd -a ubuntu www-data
-  sudo chmod -R 777 /var/www/html
-  echo '<html><center><body bgcolor="black" text="#39ff14" style="font-family: Arial"><h1>Load Balancer Demo</h1><h3>Availability Zone: ' > /var/www/html/index.html
-  curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/index.html
-  echo '</h3> <h3>Instance Id: ' >> /var/www/html/index.html
-  curl http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html
-  echo '</h3> <h3>Public IP: ' >> /var/www/html/index.html
-  curl http://169.254.169.254/latest/meta-data/public-ipv4 >> /var/www/html/index.html
-  echo '</h3> <h3>Local IP: ' >> /var/www/html/index.html
-  curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html
-  echo '</h3></html> ' >> /var/www/html/index.html
-
-
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "The page was created by the user data" | sudo tee /var/www/html/index.html
 EOF
 
   tags = {
